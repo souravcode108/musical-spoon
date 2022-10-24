e82c28f9c9cee68f333e487eb0a713da0e055d9b
souravcode108 <106593194+souravcode108@users.noreply.github.com>
2022-10-24 10:50:47 +0530

    Add files via upload
    simple java program for multiplication of two float number

---
+++ Main (1).java
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World ");
+		// Java Program to print Multiplication of two floating
+        // point Number.
+        
+        float a = 2.5f;
+        float b = 10.0f;
+        float c = a*b;
+        
+        System.out.println("the required product is:"+c);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
